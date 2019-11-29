da1541844cc4a90b8b1c530b3f318c9fce83c6ec
onera26 <32583302+onera26@users.noreply.github.com>
2019-11-29 23:39:35 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,32 @@
+import os,sys,time
+
+os.system('clear')
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('Selamat datang user di Light Cyber Indonesia Tools')
+print
+babi('Gunakan tools dengan bijak, dan mohon untuk kalian')
+print
+babi('yang tahu username & password tool ini, mohon')
+print
+babi('untuk tidak menyebarluaskan, biarkan mereka yang')
+print
+babi('usaha mencari username & passwordnya sendiri')
+print
+print
+babi('Hargai author')
+babi(' karena memakai, tidak sesulit membuat >_<')
+print
+print
+babi('Versi kali ini ada 25 tools, dan akan')
+babi(' terus update, jadi nantikan update selanjutnya')
+babi('...........')
+
+os.system('sh login.sh')
+
+exit()
